9543d057df72a73ffe0069081213dd45d1722a9e
NirHaroush <nir@baba-ent.com>
2024-12-14 14:32:37 +0200

    fixing instantiating object every iteration

--- nirharoush.java
+++ nirharoush.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 public class nirharoush {
 	public static void main(String[] args) {
+		Random rand = new Random();
 		for(int i = 1;i <= 8; i++){
-			Random rand = new Random();
 			int rand_int1 = rand.nextInt(10);
 			System.out.println(rand_int1);
 		}
